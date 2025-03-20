633cb8d25eb89a27a6cfdca1304d6fe284eeb11e
Varfolomeeva Arina <141402789+varfik@users.noreply.github.com>
2025-03-21 01:32:12 +0300

    Update dif.h
    headers added

--- dif.h
+++ dif.h
@@ -24,8 +24,9 @@ struct RigidBody
     dquat q; // rotation quaternion
 };
 
-double SolveEuler(RigidBody &rb, const Context &context, double h, double cur_time);
-
 double SolveRungeKutta(RigidBody &rb, const Context &context, double h, double cur_time);
+double SolveHeuns(RigidBody &rb, const Context &context, double h, double cur_time);
+double SolveMidPoint(RigidBody &rb, const Context &context, double h, double cur_time);
+double SolveEuler(RigidBody &rb, const Context &context, double h, double cur_time);
 
 #endif // __DIF_H
