60d4bd9643a3118c9c15249b6345efe86f2f7e42
Varfolomeeva Arina <141402789+varfik@users.noreply.github.com>
2025-03-21 01:31:06 +0300

    Update dif.cpp
    New methods were added: Heun's, MidPoint, Euler. Macros G, FLUID_DENSITY created.

--- dif.cpp
+++ dif.cpp
@@ -3,6 +3,9 @@
 #include "dif.h"
 #include "render.h"
 
+#define G 9.81
+#define FLUID_DENSITY 1000
+
 // derivative f(X(t), t) = dX/dt in point t.
 RigidBody f_rigidbody(const RigidBody &rb, const Context &context, double time)
 {
@@ -22,7 +25,7 @@ RigidBody f_rigidbody(const RigidBody &rb, const Context &context, double time)
     bool is_in_water = rb.r.y - SIZE <= 0; // water line starts in y = 0
 
     // gravity computing
-    dvec3 gravity = dvec3(0, -9.81 * (1.0 / context.M_inv), 0);
+    dvec3 gravity = dvec3(0, -G * (1.0 / context.M_inv), 0);
 
     dvec3 buoyancy = dvec3(0.0);
     dvec3 drag = dvec3(0.0);
@@ -38,7 +41,7 @@ RigidBody f_rigidbody(const RigidBody &rb, const Context &context, double time)
             displaced_fluid = pow(SIZE, 2) * std::min(height, SIZE * 1.0);
         }
         // buoyancy computing
-        buoyancy = dvec3(0.0, 1000 * 9.81 * displaced_fluid, 0.0);
+        buoyancy = dvec3(0.0, FLUID_DENSITY * G * displaced_fluid, 0.0);
         // drag computing
         drag = -50.0 * dt.r * glm::length(dt.r);
     }
@@ -85,6 +88,12 @@ RigidBody SumRB(const RigidBody &r1, const RigidBody &r2)
     return res;
 }
 
+// Runge-Kutta4
+// X(t + h) = X(t) + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+// k1 = f(X(t), t)
+// k2 = f(X(t) + h * k1 / 2, t + h / 2)
+// k3 = f(X(t) + h * k2 / 2, t + h / 2)
+// k4 = f(X(t) + h * k3, t + h)
 
 double SolveRungeKutta(RigidBody &rb, const Context &context, double h, double cur_time)
 {
@@ -94,6 +103,55 @@ double SolveRungeKutta(RigidBody &rb, const Context &context, double h, double c
     RigidBody k4 = f_rigidbody(SumRB(rb, MulRB(k3, h)), context, cur_time + h);
 
     rb = SumRB(rb, MulRB(SumRB(k1, SumRB(MulRB(k2, 2), SumRB(MulRB(k3, 2), k4))), h / 6));
+
+    dmat3 R = dmat3(glm::normalize(rb.q));
+    dvec3 omega = R * context.I_inv * glm::transpose(R) * rb.L;
+
+    return glm::dot(omega, rb.L);
+}
+
+// Heun's method
+// X(t + h) = X(t) + h / 2 * (k1 + k2)
+// k1 = f(X(t), t)
+// k2 = f(X(t) + h * k1, t + h)
+    
+double SolveHeuns(RigidBody &rb, const Context &context, double h, double cur_time)
+{
+    RigidBody k1 = f_rigidbody(rb, context, cur_time);
+    RigidBody k2 = f_rigidbody(SumRB(rb, MulRB(k1, h)), context, cur_time + h);
+
+    rb = SumRB(rb, MulRB(SumRB(k1, k2), h / 2));
+
+    dmat3 R = dmat3(glm::normalize(rb.q));
+    dvec3 omega = R * context.I_inv * glm::transpose(R) * rb.L;
+
+    return glm::dot(omega, rb.L);
+}
+
+// MidPoint method
+// X(t + h) = X(t) + h * k2
+// k1 = f(X(t), t)
+// k2 = f(X(t) + h * k1 / 2, t + h / 2)
+
+double SolveMidPoint(RigidBody &rb, const Context &context, double h, double cur_time)
+{
+    RigidBody k1 = f_rigidbody(rb, context, cur_time);
+    RigidBody k2 = f_rigidbody(SumRB(rb, MulRB(k1, h / 2)), context, cur_time + h / 2);
+
+    rb = SumRB(rb, MulRB(k2, h));
+
+    dmat3 R = dmat3(glm::normalize(rb.q));
+    dvec3 omega = R * context.I_inv * glm::transpose(R) * rb.L;
+
+    return glm::dot(omega, rb.L);
+}
+
+// Euler's method
+// X(t + h) = X(t) + h * f(X(t), t)
+
+double SolveEuler(RigidBody &rb, const Context &context, double h, double cur_time)
+{
+    rb = SumRB(rb, MulRB(f_rigidbody(rb, context, cur_time), h));
 
     dmat3 R = dmat3(glm::normalize(rb.q));
     dvec3 omega = R * context.I_inv * glm::transpose(R) * rb.L;
